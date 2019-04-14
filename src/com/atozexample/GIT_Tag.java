f41a299dd9d0e1fa2a5973fa0f3e5d09f1c4a27c
mukeshkumar1234 <mk4197762@gmail.com>
2019-04-15 00:37:17 +0530

    ms

--- src/com/atozexample/GIT_Tag.java
+++ src/com/atozexample/GIT_Tag.java
@@ -10,6 +10,8 @@ public class GIT_Tag {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("hello branch2.0");
+		System.out.println("master");
+		
 	}
 
 }
